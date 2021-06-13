5d8b98c0d18c8422ec56b08d3377c7e03c4c0cdb
cvcastano <carocastanocastillo@gmail.com>
2021-06-13 19:00:18 +0200

    Improve PageHeader component and add stylesheet

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -1,12 +1,13 @@
-import '../components/pages/Register.scss';
+import './PageHeader.scss';
 
 function PageHeader(props) {
     return (
-        <section className={`${props.name}`}>
-            <p className={`${props.name}_notice`}>{props.notice}</p>
-            <div className={`${props.name}_text`}>
-                <h2 className={`${props.name}_text-title`}>{props.title}</h2>
-                <p className={`${props.name}_text-paragraph`}>{props.paragraph}</p>
+        <section className="page_header">
+            <p className="page_header_notice">{props.notice}</p>
+            <p className="page_header_name">{props.name}</p>
+            <div className="page_header_text">
+                <h2 className="page_header_text-title">{props.title}</h2>
+                <p className="page_header_text-paragraph">{props.paragraph}</p>
             </div>
         </section>
     );
