4e091f912742465b90a2e720583aef89ce8f2851
cvcastano <carocastanocastillo@gmail.com>
2021-06-14 01:33:16 +0200

    Add Location page and styles

--- src/components/pages/Location.js
+++ src/components/pages/Location.js
@@ -1,15 +1,44 @@
 import PageHeader from '../PageHeader';
+import InputGroup from '../InputGroup';
 import './Location.scss';
 
 function Location(props) {
     return (
         <>
             <PageHeader
-                name='location'
+                url='/register/personal_info'
+                name='Localización'
                 notice='paso 02/03'
                 title='Completa tu perfil'
                 paragraph='Para poder revisar que se trata de una cuenta real, necesitamos la siguiente información'
             />
+            <form action="" method="post" className="form">
+                <InputGroup
+                    id='tel'
+                    text='Número de teléfono'
+                    placeholder=''
+                    type='tel'
+                    name='tel'
+                />
+                <InputGroup
+                    id='address'
+                    text='Dirección'
+                    placeholder='Introduce la dirección completa'
+                    type='text'
+                    name='address'
+                />
+                <InputGroup
+                    id='country'
+                    text='País de residencia'
+                    placeholder='Selecciona uno'
+                    type='text'
+                    name='country'
+                />
+                <button type="button" className="save_button">
+                    Guardar y continuar
+                </button>
+                <i className="fas fa-user-lock"> Tu información es segura</i>
+            </form>
         </>
     );
 }
