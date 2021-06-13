e748e6a2480595a89fa7e1a357a3b29b8cb3d153
cvcastano <carocastanocastillo@gmail.com>
2021-06-13 10:02:25 +0200

    Create InputGroup component

---
+++ src/InputGroup.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './InputGroup.scss';
+
+function InputGroup(props) {
+
+    return (
+        <>
+                    <label className="label" htmlFor={props.id}>
+                        {props.text}
+                    </label>
+                    <input
+                        className="input"
+                        placeholder={props.placeholder}
+                        type={props.type}
+                        name={props.name}
+                        defaultValue={props.value}
+                    />
+        </>
+                );
+}
+
+                export default InputGroup;
